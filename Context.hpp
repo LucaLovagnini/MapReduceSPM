dc1b148d3f1c820aec1833c33ab68b28d486c8c0
Luca Lovagnini <lucalova91@gmail.com>
2016-01-09 19:26:45 +0100

    test

--- Context.hpp
+++ Context.hpp
@@ -8,7 +8,7 @@
 #ifndef CONTEXT_HPP_
 #define CONTEXT_HPP_
 
-#define SIZE 4
+#define SIZE 128
 
 #include <array>
 #include "MapReduceWorker.hpp"
@@ -20,6 +20,8 @@ class Context{
 public:
 	Context(MapReduceWorker<MIK,MIV,MOK,MOV> *worker, const unsigned short nWorkers) : worker(worker),nWorkers(nWorkers){
 		inter_values = new vector<pair<MOK,MOV>>[nWorkers];
+		for(int i=0;i<nWorkers;i++)
+		    inter_values[i].reserve(SIZE);
 	}
 
 	void emit(MOK key, MOV value) {
@@ -29,11 +31,13 @@ public:
 		s<<"key="<<key<<" red_worker="<<red_worker<<endl;
 		cout<<s.str();*/
 		inter_values[red_worker].push_back({key,value});
-		if(inter_values[red_worker].size()>=SIZE)
+		if(inter_values[red_worker].size()>=SIZE){
 			sort_append_clear(red_worker);
+		}
 	}
 
 	void flush(){
+		cout<<"flushing"<<endl;
 		for(int i=0;i<nWorkers;i++)
 			if(!inter_values[i].empty())
 				sort_append_clear(i);
@@ -45,18 +49,19 @@ public:
 
 	void sort_append_clear(const unsigned short red_worker){
 		stringstream s;
-		sort(inter_values[red_worker].begin(),inter_values[red_worker].end(),[]( const std::pair<MOK,MOV> &left,  const std::pair<MOK,MOV> &right) {
+		/*sort(inter_values[red_worker].begin(),inter_values[red_worker].end(),[]( const std::pair<MOK,MOV> &left,  const std::pair<MOK,MOV> &right) {
 		    return left.second < right.second;
-		});
+		});*/
 		//worker->all_mtx[red_worker].lock();
 	    if (worker->all_inter_values[red_worker].empty())
 	        worker->all_inter_values[red_worker] = move(inter_values[red_worker]);
 	    else{
-	        worker->all_inter_values[red_worker].reserve(worker->all_inter_values[red_worker].size() + inter_values[red_worker].size());
+	        //worker->all_inter_values[red_worker].reserve(worker->all_inter_values[red_worker].size() + inter_values[red_worker].size());
 	        move(begin(inter_values[red_worker]), end(inter_values[red_worker]), back_inserter(worker->all_inter_values[red_worker]));
 	    }
 		//worker->all_mtx[red_worker].unlock();
 	    inter_values[red_worker].clear();
+	    inter_values[red_worker].reserve(SIZE);
 	}
 private:
 	vector<pair<MOK,MOV>> *inter_values;
